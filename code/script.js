da2f61733518bcfb9b6ddb3e5ce7a9515e3100bc
marwal1987 <marwal1987@gmail.com>
2023-11-17 10:13:24 +0100

    changed language in comment

--- code/script.js
+++ code/script.js
@@ -2,7 +2,7 @@
 function toggle() {
   this.classList.toggle("active");
 }
-// -- Endast part 1 --
+// -- Only part 1 --
 // Selects and HTML element, and calls a function which will be executed when the element is clicked.
 // document.getElementById("section1").onclick = toggle;
 // document.getElementById("section2").onclick = toggle;
